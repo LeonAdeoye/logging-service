f0fea8abb21c8881e43a5c92e11029e8e40a9231
LeonAdeoye <leon.adeoye@gmail.com>
2020-04-08 19:41:12 +0800

    added application.properties

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,9 @@
 package com.leon;
 
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
-import java.util.Arrays;
 
 @SpringBootApplication
 public class Application
