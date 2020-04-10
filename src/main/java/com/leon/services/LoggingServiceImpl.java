59e09923877fcf2b7e5e69a40cb1e96f92a4de3d
LeonAdeoye <leon.adeoye@gmail.com>
2020-04-10 22:25:03 +0800

    added logger status print

--- src/main/java/com/leon/services/LoggingServiceImpl.java
+++ src/main/java/com/leon/services/LoggingServiceImpl.java
@@ -1,51 +1,27 @@
 package com.leon.services;
-import org.slf4j.Logger;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.rolling.TimeBasedRollingPolicy;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.stereotype.Service;
-
 import java.util.HashMap;
 import java.util.Map;
-
 import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.core.util.StatusPrinter;
-
-import javax.annotation.PostConstruct;
+import ch.qos.logback.core.rolling.RollingFileAppender;
+import ch.qos.logback.classic.Logger;
 
 @Service
 public class LoggingServiceImpl implements LoggingService
 {
-    private Map<String,Logger> loggers = new HashMap<>();
-
-    @PostConstruct
-    public void initialize()
-    {
-        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
-        StatusPrinter.print(lc);
-    }
+    private static Map<String,Logger> loggerMap = new HashMap<>();
+    private LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
 
     @Override
     public void log(String logger, String level, String message)
     {
-        Logger loggerInstance = getLogger(logger);
-        logMessage(loggerInstance, level, message);
-    }
-
-    private Logger getLogger(String logger)
-    {
-        Logger loggerInstance;
-
-        if(loggers.containsKey(logger))
-        {
-            loggerInstance = loggers.get(logger);
-        }
-        else
-        {
-            loggerInstance =  LoggerFactory.getLogger(logger);
-            loggers.put(logger, loggerInstance);
-        }
-
-        return loggerInstance;
+        logMessage(getLogger(logger), level, message);
     }
 
     private void logMessage(Logger loggerInstance, String level, String message)
@@ -68,5 +44,42 @@ public class LoggingServiceImpl implements LoggingService
                 loggerInstance.error(message);
                 break;
         }
+    }
+
+    synchronized private Logger getLogger(String loggerName)
+    {
+        if(!loggerMap.containsKey(loggerName))
+            loggerMap.put(loggerName, createLoggerInstance(loggerName));
+
+        return loggerMap.get(loggerName);
+    }
+
+    private Logger createLoggerInstance(String loggerName)
+    {
+        RollingFileAppender<ILoggingEvent> appender = new RollingFileAppender<>();
+        TimeBasedRollingPolicy rollingPolicy = new TimeBasedRollingPolicy();
+        rollingPolicy.setFileNamePattern("%d{yyyy-MM-dd_HH-mm}.log");
+        rollingPolicy.setParent(appender);
+        rollingPolicy.setContext(loggerContext);
+        rollingPolicy.start();
+
+        PatternLayoutEncoder encoder = new PatternLayoutEncoder();
+        encoder.setContext(loggerContext);
+        encoder.setPattern("%msg%n");
+        encoder.start();
+
+        appender.setContext(loggerContext);
+        appender.setRollingPolicy(rollingPolicy);
+        appender.setEncoder(encoder);
+        appender.setFile(loggerName);
+        appender.setAppend(true);
+        appender.start();
+
+        Logger loggerInstance = (Logger) LoggerFactory.getLogger(loggerName);
+        loggerInstance.addAppender(appender);
+        loggerInstance.setLevel(Level.TRACE);
+        loggerInstance.setAdditive(true);
+
+        return loggerInstance;
     }
 }
