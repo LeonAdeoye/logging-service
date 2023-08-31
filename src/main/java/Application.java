83fd838f0e4fdaef59fae9cd758a54a9f01018f0
LeonAdeoye <leon.adeoye@gmail.com>
2023-08-31 18:22:19 +0800

    Added Consul configs

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@EnableDiscoveryClient
 public class Application
 {
     public static void main(String[] args)
