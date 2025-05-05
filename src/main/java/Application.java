2fbe7713b40538452e2a1d92d314fd76363fd092
LeonAdeoye <leon.adeoye@gmail.com>
2025-05-05 19:01:48 +0800

    remove consul

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
-@EnableDiscoveryClient
 public class Application
 {
     public static void main(String[] args)
